4333211d4740af677b25505f627099fc249dfea6
Tristan Pinaudeau <tristan@tic.sh>
2021-04-04 19:08:34 +0200

    debt: refactor tests and add test for users

--- test.sh
+++ test.sh
@@ -2,16 +2,20 @@
 
 URL_BASE=http://localhost:5000
 
+success() { echo -e "\e[32mOK   $1\e[0m"; }
+failure() { echo -e "\e[31mFAIL $1\e[0m"; }
+
 db_query() {
   docker-compose -p dev-backend exec database \
     psql --csv -U postgres -d gallery -c "$1"
 }
 
 be_query() {
+  data=$([[ "$1" == "POST" || "$1" == "PUT" ]] && echo -d $3)
   curl -sw "%{http_code}" \
     -H 'Content-Type: application/json' \
     -H 'Accept: application/json' \
-    -X $1 "$URL_BASE/$2" -d $3
+    -X $1 "$URL_BASE/$2" $data; echo
 }
 
 test_create_user() {
@@ -22,15 +26,26 @@ test_create_user() {
   second_code=$(
     be_query 'POST' 'users' '{"name":"tristan","email":"tristan@tic.sh"}'
   )
-  insert=$(\
-    db_query 'select * from public.user' | wc -l\
-  )
+  insert=$(db_query 'select * from public.user' | wc -l)
   if [[ $insert -eq 2 && $first_code -eq 204 && $second_code -eq 409 ]]; then
-    echo 'OK test_create_user'
+    success 'test_create_user'
   else
-    echo 'FAIL test_create_user'
+    failure 'test_create_user'
+  fi
+}
+
+test_list_users() {
+  make reset &> /dev/null
+  result=$(be_query 'GET' 'users')
+  body=$(echo $result | head -c -4)
+  code=$(echo $result | tail -c 4)
+  if [[ $code == 200 ]]; then
+    success 'test_list_users'
+  else
+    failure 'test_list_users'
   fi
 }
 
 make down &> /dev/null && make up &> /dev/null
 test_create_user
+test_list_users
